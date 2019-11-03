f1a2c4ce0a60121968768ce47f92084a1d53e3cd
nanukakapanadze <nanuka.kapanadze.1@btu.edu.ge>
2019-11-03 22:01:04 +0400

    my project

---
+++ src/BTU/Main.java
@@ -0,0 +1,36 @@
+package BTU;
+public class Main {
+    public static void main(String[] args) {
+        String [] a = {"C++","C#","java","pascal","php","JavaScript","ActionScript"};
+        for (int i=0; i < a.length; i++){
+            System.out.println(a[i]);
+        }
+    }
+}
+
+//package BTU;
+//public class Main {
+//    public static void main(String[] args) {
+//        String[] saxelebi = {"nanuka","anuka","manana","malxazi","nanuli"};
+//        for (int i=0; i<saxelebi.length; i++){
+//            System.out.println(saxelebi[i]);
+//        }
+//        System.out.println(saxelebi[25]);
+//    }
+//}
+
+
+//1
+//package BTU;
+//public class Main {
+//    public static void main(String[] args) {
+////        int a = 1;
+////        while (a < 10){
+////            System.out.println("hello world "+a);
+////            a++;
+//        for(int a = 2; a < 11; a+=2){
+//            System.out.println("hello world "+a);
+//        }
+//        System.out.println("THE END");
+//    }
+//}
